5465615ca8715a7839930e6fa626faa7f4ea8a92
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-02-11 17:23:53 +0330

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import click
+from pathlib import Path
+from tidy import File, Folder
+
+
+@click.command()
+@click.option("-i", "--input", "input_path",
+              help="Input directory or file.",
+              required=True,
+              type=click.Path(
+                  exists=True,
+                  path_type=Path
+              ))
+@click.option("-o", "--output", "output_path",
+              help="Output directory.",
+              default="output",
+              type=click.Path(
+                  file_okay=False,
+                  writable=True,
+                  path_type=Path
+              ))
+@click.option("-f", "--format", "format_pattern",
+              help="Folders format.",
+              default="%Y/%B/%d",
+              show_default=True,
+              type=click.STRING)
+def main(input_path: Path, output_path: Path, format_pattern: str):
+    files = []
+    if input_path.is_file():
+        files = [File(input_path)]
+    if input_path.is_dir():
+        files = Folder(input_path).sub_files()
+
+    for file in files:
+        path = output_path / file.creation_date.strftime(format_pattern)
+        path.mkdir(parents=True, exist_ok=True)
+        file.copy(path)
+    click.echo(output_path)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
---
+++ tidy/core.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import shutil
+import datetime
+
+
+class File:
+
+    def __init__(self, path):
+        if not path.is_file():
+            raise ValueError
+        self.path = path
+
+    @property
+    def creation_date(self):
+        ctime = datetime.datetime.fromtimestamp(self.path.stat().st_ctime)
+        return ctime
+
+    def copy(self, path):
+        shutil.copy2(self.path, path)
+
+    def __str__(self):
+        return self.path.name
+
+
+class Folder:
+
+    def __init__(self, path, make=True):
+        if make:
+            path.mkdir(parents=True, exist_ok=True)
+        if not path.is_dir():
+            raise ValueError
+        self.path = path
+
+    def sub_files(self):
+        all_sub_paths = self.path.glob('**/*')
+        files = [File(path) for path in all_sub_paths if path.is_file()]
+        return files
