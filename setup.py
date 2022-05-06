12f3f34745d5dc4397ec5213bfd89027728a5c5c
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-05-06 13:09:05 +0430

    Update version 0.5.1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="Tidier",
-    version="0.5",
+    version="0.5.1",
     packages=find_packages(),
     url="https://github.com/itsamirhn/Tidier",
-    download_url="https://github.com/itsamirhn/Tidier/archive/refs/tags/v0.5.tar.gz",
+    download_url="https://github.com/itsamirhn/Tidier/archive/refs/tags/v0.5.1.tar.gz",
     license="MIT",
     author="AmirMohammad Hosseini Nasab",
     author_email="awmirhn@gmail.com",
