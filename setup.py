20bd951174193084789c95eda71e438ba41ebc55
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-08-17 09:49:58 +0430

    New Version v0.5.4

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup, find_packages
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def license():
+    with open('LICENSE.txt') as f:
+        return f.read()
+
+
 setup(
     name="Tidier",
-    version="0.5.3",
+    version="0.5.4",
     packages=find_packages(),
     url="https://github.com/itsamirhn/Tidier",
-    download_url="https://github.com/itsamirhn/Tidier/archive/refs/tags/v0.5.3.tar.gz",
-    license="MIT",
+    download_url="https://github.com/itsamirhn/Tidier/archive/refs/tags/v0.5.4.tar.gz",
+    license=license(),
     author="AmirMohammad Hosseini Nasab",
     author_email="awmirhn@gmail.com",
     description="A tool for organizing files",
+    long_description=readme(),
     install_requires=[
         "Click",
         "jdatetime",
