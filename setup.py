6fe12331ea566d3d69688fce0c1606761299f258
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-08-17 09:19:41 +0430

    Fix requirements in version 0.5.3

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="Tidier",
-    version="0.5.2",
+    version="0.5.3",
     packages=find_packages(),
     url="https://github.com/itsamirhn/Tidier",
-    download_url="https://github.com/itsamirhn/Tidier/archive/refs/tags/v0.5.2.tar.gz",
+    download_url="https://github.com/itsamirhn/Tidier/archive/refs/tags/v0.5.3.tar.gz",
     license="MIT",
     author="AmirMohammad Hosseini Nasab",
     author_email="awmirhn@gmail.com",
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         "Click",
         "jdatetime",
+        "pathlib",
     ],
     entry_points={
         "console_scripts": [
